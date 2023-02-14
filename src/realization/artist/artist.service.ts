97e12e18935e65a03d5416325c02eaff66f0172c
s_iceman <s_iceman@protonmail.com>
2023-02-14 23:51:55 +0300

    feat: support table ARTIST

--- src/realization/artist/artist.service.ts
+++ src/realization/artist/artist.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { InvalidUuid } from 'src/common/exceptions';
+import { InvalidUuid, NotFound } from 'src/common/exceptions';
 import { DatabaseService } from 'src/database/db.provider';
 import { IArtist } from 'src/database/interfaces/artist.interface';
 import { validate } from 'uuid';
@@ -10,31 +10,47 @@ export class ArtistService {
   constructor(private db: DatabaseService) {}
 
   async getArtists(): Promise<IArtist[]> {
-    return await this.db.artists.getArtists();
+    return await this.db.artist.findMany();
   }
 
   async getArtist(id: string): Promise<IArtist> {
     if (!validate(id)) {
       throw new InvalidUuid();
     }
-    return await this.db.artists.getArtist(id);
+    try {
+      return await this.db.artist.findUniqueOrThrow({ where: { id } });
+    } catch (err) {
+      throw new NotFound();
+    }
   }
 
   async createArtist(createArtistDto: ArtistDto): Promise<IArtist> {
-    return await this.db.artists.createArtist(createArtistDto);
+    return await this.db.artist.create({ data: createArtistDto });
   }
 
   async deleteArtist(id: string): Promise<void> {
     if (!validate(id)) {
       throw new InvalidUuid();
     }
-    await this.db.deleteArtist(id);
+    try {
+      await this.db.artist.delete({ where: { id } });
+    } catch (err) {
+      throw new NotFound();
+    }
   }
 
   async updateArtist(id: string, updateArtistDto: ArtistDto): Promise<IArtist> {
     if (!validate(id)) {
       throw new InvalidUuid();
     }
-    return await this.db.artists.updateArtist(id, updateArtistDto);
+
+    try {
+      return await this.db.artist.update({
+        where: { id },
+        data: updateArtistDto,
+      });
+    } catch (err) {
+      throw new NotFound();
+    }
   }
 }

ac13007591401daa455fbde577c7feb7fed49230
s_iceman <s_iceman@protonmail.com>
2023-02-16 00:12:11 +0300

    feat: support table TRACK

--- src/realization/track/track.service.ts
+++ src/realization/track/track.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { InvalidUuid } from '../../common/exceptions';
+import { InvalidUuid, NotFound } from '../../common/exceptions';
 import { DatabaseService } from '../../database/db.provider';
 import { ITrack } from '../../database/interfaces/track.interface';
 import { validate } from 'uuid';
@@ -10,31 +10,46 @@ export class TrackService {
   constructor(private db: DatabaseService) {}
 
   async getTracks(): Promise<ITrack[]> {
-    return await this.db.tracks.getTracks();
+    return await this.db.track.findMany();
   }
 
   async getTrack(id: string): Promise<ITrack> {
     if (!validate(id)) {
       throw new InvalidUuid();
     }
-    return await this.db.tracks.getTrack(id);
+    try {
+      return await this.db.track.findUnique({ where: { id }});
+    } catch (err) {
+      throw new NotFound();
+    }
   }
 
   async createTrack(createTrackDto: TrackDto): Promise<ITrack> {
-    return await this.db.tracks.createTrack(createTrackDto);
+    return await this.db.track.create({ data: createTrackDto });
   }
 
   async deleteTrack(id: string): Promise<void> {
     if (!validate(id)) {
       throw new InvalidUuid();
     }
-    await this.db.deleteTrack(id);
+    try {
+      await this.db.track.delete({ where: { id } });
+    } catch (err) {
+      throw new NotFound();
+    }
   }
 
   async updateTrack(id: string, updateTrackDto: TrackDto): Promise<ITrack> {
     if (!validate(id)) {
       throw new InvalidUuid();
     }
-    return await this.db.tracks.updateTrack(id, updateTrackDto);
+    try {
+      return await this.db.track.update({
+        where: { id },
+        data: { updateTrackDto },
+      });
+    } catch (err) {
+      throw new NotFound();
+    }
   }
 }

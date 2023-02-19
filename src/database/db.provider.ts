a93c4972a6bbb3ec417aaff38b1084aa958d7541
s_iceman <s_iceman@protonmail.com>
2023-02-19 19:55:26 +0300

    fix: add hook to close the application

--- src/database/db.provider.ts
+++ src/database/db.provider.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, INestApplication } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class DatabaseService extends PrismaClient {
   constructor() {
     super();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      console.log('beforeExit hook');
+      await app.close();
+    });
   }
 }
